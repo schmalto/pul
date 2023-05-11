7298c236e7534df67f55aab2bdf09efb3808993e
Tobias S <65611153+B4ldur-ts@users.noreply.github.com>
2023-05-12 00:27:04 +0200

    updates

--- resnet_test.py
+++ resnet_test.py
@@ -1,36 +1,46 @@
 import tensorflow as tf
 from tensorflow import keras
+from keras_cv.visualization import plot_image_gallery
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras_cv.models import YOLOV8Detector, YOLOV8Backbone
-from resnet_helper import create_dataset
-
-train_path = 'data/train'
-valid_path = 'data/valid'
-
-dataset_path = 'data'
+from resnet_helper import get_data_set
+import keras_cv
 
 
+train_path = '/home/tobias/git_ws/pul/val/buffalo/'
+valid_path = '/home/tobias/git_ws/pul/val/buffalo/'
 
-train_dataset, train_labels, valid_dataset, valid_labels = create_dataset(dataset_path)
+
+train_dataset, train_labels = get_data_set(train_path)
+valid_dataset, valid_labels = get_data_set(valid_path)
+
+plot_image_gallery(train_dataset, value_range=(0, 255))
+
+
+resized = keras_cv.layers.Resizing(180, 180, bounding_box_format="xywh", pad_to_aspect_ratio=True)(train_dataset)
+
 
 
 # Set up the model
 model = YOLOV8Detector(
-    num_classes=5, # Number of object classes
-    bounding_box_format='corners', # Format of bounding boxes: 'corners' or 'minmax'
+    num_classes=20, # Number of object classes
+    bounding_box_format='xywh', # Format of bounding boxes: 'corners' or 'minmax'
     backbone=YOLOV8Backbone.from_preset(
-        "yolo_v8_m_backbone"
+        #"yolo_v8_m_backbone"
+        "yolo_v8_m_backbone_coco"
     ),
     fpn_depth=2,
 )
 
 # Compile the model
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+    #optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+    optimizer=tf.optimizers.SGD(global_clipnorm=10.0),
     #box_loss='mse',
     box_loss='iou',
     # box_loss='giou',
     classification_loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    jit_compile=False,
 )
 
 # Set up training callbacks
@@ -43,12 +53,11 @@ callbacks = [
 # Train the model
 
 model.fit(
-    train_dataset,
-    train_labels,
-    epochs=100,
-    callbacks=callbacks,
-    validation_data=(valid_dataset, valid_labels),
-
+    (train_dataset,
+    train_labels),
+    # epochs=100,
+    # callbacks=callbacks,
+    # validation_data=(valid_dataset, valid_labels),
 )
 
 
--- test.py
+++ test.py
@@ -1,27 +1,12 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 import numpy as np
-import os
-import tensorflow_datasets
-import pathlib
 from termcolor import colored
 from tqdm import tqdm
 import glob
 
 
-
-
-
-def works():
-    img = Image.open('301.jpg')
-    img_to_tensor = tf.convert_to_tensor(img)
-    arr_ = np.squeeze(img_to_tensor.numpy())
-    plt.imshow(arr_)
-    plt.show()
-
-def labels():
+def get_labels(data_dir):
     # train_labels = {
     #     "boxes": [
     #         [
@@ -42,12 +27,12 @@ def labels():
             
         ]
     }
-    for name in glob.glob('/home/tobias/git_ws/pul/train/buffalo/*.txt'):
+    for name in glob.glob(data_dir + '*.txt'):
         with open(name) as f:
             lines = f.readlines()
             classes_in_file =[]
             boxes_in_file = []
-            for line in lines:
+            for line in tqdm(lines):
                 line = line.split()
                 class_name = line[0]
                 x_min = line[1]
@@ -58,59 +43,38 @@ def labels():
                 boxes_in_file.append([x_min, y_min, x_max, y_max])
             train_labels["classes"].append(classes_in_file)
             train_labels["boxes"].append(boxes_in_file)
-    print(train_labels)
 
 
-def to_test():
+def get_data_set(data_dir):
+    train_images = get_image_data(data_dir)
+    train_labels = get_labels(data_dir)
+    return train_images, train_labels
+
+
+def get_image_data(data_dir):
     img_height = 180
     img_width = 180
     batch_size = None
-    dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-    archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
-    data_dir = pathlib.Path(archive).with_suffix('')
 
-    train_labels = {}
-    valid_labels = {}
 
     train_ds = tf.keras.utils.image_dataset_from_directory(
         data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
         image_size=(img_height, img_width),
         batch_size=batch_size,
-
     )
     
     train_ds.map(lambda x, y: (tf.cast(x, tf.float32), y))
-    
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size,)
-    
-    val_ds.map(lambda x, y: (tf.cast(x, tf.float32), y))
-
     train_images = np.zeros((1, 180, 180, 3))
-    valid_images = np.zeros((1, 180, 180, 3))
    
     for image_batch, labels_batch in tqdm(train_ds):
         image_batch = tf.expand_dims(image_batch, axis=0)
         image_batch = image_batch.numpy()
         train_images = np.append(train_images, image_batch, axis=0)
 
-    for image_batch, labels_batch in tqdm(val_ds):
-        image_batch = tf.expand_dims(image_batch, axis=0)
-        image_batch = image_batch.numpy()
-        valid_images = np.append(valid_images, image_batch, axis=0)
-
     train_images = tf.convert_to_tensor(train_images, dtype=tf.int32)
-    valid_images = tf.convert_to_tensor(valid_images, dtype=tf.int32)
 
-    return train_images, train_labels, valid_images, valid_labels
+    return train_images
+
 
 if __name__ == '__main__':
-    labels()
+    labels('/home/tobias/git_ws/pul/train/buffalo/')
