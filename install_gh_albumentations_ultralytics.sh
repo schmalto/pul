dc7a39860d0af265d81a8f375affeca0057ea1b3
Tobias S <65611153+B4ldur-ts@users.noreply.github.com>
2023-08-02 19:52:33 +0200

    Update install_gh_albumentations_ultralytics.sh

--- install_gh_albumentations_ultralytics.sh
+++ install_gh_albumentations_ultralytics.sh
@@ -1,10 +1,17 @@
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+LIGHT_CYAN='\033[1;36m'
+RED='\033[0;31m'
+
+
+
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
 
 
-echo "[Setup] Installing Github CLI"
+echo -e "${LIGHT_CYAN}[Setup]${NC} Installing Github CLI"
 
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -13,10 +20,10 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo apt update \
 && sudo apt install gh -y
 
-echo "[Setup] Installed Github CLI"
-echo "[Setup] Installing Ultralytics"
+echo -e "${LIGHT_CYAN}[Setup]${NC} Installed Github CLI"
+echo -e "${LIGHT_CYAN}[Setup]${NC} Installing Ultralytics"
 pip install ultralytics
-echo "[Setup] Installed Ultralytics"
-echo "[Setup] Installing Albumentations"
+echo -e "${LIGHT_CYAN}[Setup]${NC} Installed Ultralytics"
+echo -e "${LIGHT_CYAN}[Setup]${NC} Installing Albumentations"
 pip install albumentations
-echo "[Setup] Installed Albumentations"
+echo -e "${LIGHT_CYAN}[Setup]${NC} Installed Albumentations"
