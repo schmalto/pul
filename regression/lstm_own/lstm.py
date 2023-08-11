40479ec8630ed1b91d2c30240f0a4a608d20c3aa
Tobias S <65611153+B4ldur-ts@users.noreply.github.com>
2023-08-12 00:22:19 +0200

    further improvements

--- regression/lstm_own/lstm.py
+++ regression/lstm_own/lstm.py
@@ -1,7 +1,12 @@
 import torch.nn as nn
+import torch.optim as optim
+import torch
+from tqdm import tqdm
+import numpy as np
 
 
 class AirModel(nn.Module):
+
     def __init__(self, input_dim=1, hidden_dim=50, num_layers=1, bidirectional=True, dense=True):
         super().__init__()
         self.bidirectional = bidirectional
@@ -22,13 +27,48 @@ class AirModel(nn.Module):
             self.final = nn.Linear(self.hidden_dim, 1)
         self.act2 = nn.ReLU()
 
+        self.optimizer = optim.Adam(self.parameters(), lr=0.001)
+        self.loss_fn = nn.MSELoss()
+
     def forward(self, x):
         x = x.to(self.lstm.weight_ih_l0.dtype)  # Convert x to the same data type as LSTM weights
         x, _ = self.lstm(x)
-        x = x[:, -1, :]
+        # x = x[:, -1, :]
         x = self.act1(x)
         if self.dense:
             x = self.linear(x)
             x = self.act2(x)
         x = self.final(x)
         return x
+
+    def training_2(self, device, dataset, loader, n_epochs=1000):
+        test_loss = []
+        train_loss = []
+        X_train = dataset[0]
+        y_train = dataset[1]
+        X_test = dataset[2]
+        y_test = dataset[3]
+        for epoch in tqdm(range(n_epochs)):
+            self.train()
+            for X_batch, y_batch in loader:
+                self.optimizer.zero_grad()
+                y_pred = self(X_batch.to(device))
+                loss = self.loss_fn(y_pred.to(device), y_batch.to(device))
+                train_loss.append(loss)
+                loss.backward()
+                self.optimizer.step()
+            # Validation
+            if epoch % 100 != 0:
+                continue
+            self.eval()
+            with torch.no_grad():
+                y_pred = self(X_train.to(device))
+                train_rmse = np.sqrt(self.loss_fn(y_pred.to(device), y_train.to(device)))
+                y_pred = self(X_test.to(device))
+                test_rmse = np.sqrt(self.loss_fn(y_pred.to(device), y_test.to(device)))
+                if test_rmse < min(test_loss):
+                    torch.save(self, 'ltsm_best.pt')
+                test_loss.append(test_rmse)
+
+            print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+            torch.save(self, 'ltsm_last.pt')
--- regression/lstm_own/training.py
+++ regression/lstm_own/training.py
@@ -3,33 +3,33 @@ import pandas as pd
 import torch
 import numpy as np
 from lstm import AirModel
-import torch.optim as optim
 import torch.utils.data as data
-from torch import nn
 from tqdm import tqdm
 from lstm_utils import weeks, days, hours, months
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-lookback = 1
-hidden_dim = 32
-num_layers = 1
-bidirectional = True
-dense = True
-input_dim = 1
-batch_size = lookback
+def load_dataset(lookback):
+    df = pd.read_csv('lorry_data.csv')
+    # Filter out the error-prone range
+    timeseries = df[['lorry_free']].values.astype('float32')
+    mask = np.ones_like(timeseries, dtype=bool)
+    mask[101700:242500] = False
+    timeseries = timeseries[mask]
+    # plt.xlabel('Time in minutes since 10.06.2022 14:58')
+    # plt.ylabel('Lorry free')
+    # plt.plot(timeseries)
+    # plt.show()
 
+    train_size = int(len(timeseries) * 0.67)
+    test_size = len(timeseries) - train_size
+    train, test = timeseries[:train_size], timeseries[train_size:]
 
-df = pd.read_csv('lorry_data.csv')
-timeseries = df[['lorry_free']].values.astype('float32')
-# plt.xlabel('Time in minutes since 10.06.2022 14:58')
-# plt.ylabel('Lorry free')
-# plt.plot(timeseries)
-# plt.show()
+    X_train, y_train = create_dataset(train, lookback=lookback)
+    X_test, y_test = create_dataset(test, lookback=lookback)
 
-train_size = int(len(timeseries) * 0.67)
-test_size = len(timeseries) - train_size
-train, test = timeseries[:train_size], timeseries[train_size:]
+    dataset = [X_train, y_train, X_test, y_test]
+
+    return dataset, train_size, test_size, timeseries
 
 
 def create_dataset(dataset, lookback):
@@ -47,87 +47,92 @@ def create_dataset(dataset, lookback):
         y.append(target)
     X = np.array(X)
     y = np.array(y)
+    # Normalize values
+    X = X / 28
+    y = y / 28
     return torch.tensor(X), torch.tensor(y)
 
 
-X_train, y_train = create_dataset(train, lookback=lookback)
-X_test, y_test = create_dataset(test, lookback=lookback)
+def train_model(n_epochs, dataset, device, batch_size, input_dim, hidden_dim, num_layers, bidirectional, dense):
+    model_y = AirModel(input_dim=input_dim, hidden_dim=hidden_dim, num_layers=num_layers, bidirectional=bidirectional,
+                     dense=dense).to(device)
+    X_train, y_train, _, _ = dataset
+    loader = data.DataLoader(data.TensorDataset(X_train, y_train), shuffle=True, batch_size=batch_size)
 
-model = AirModel(input_dim=input_dim, hidden_dim=hidden_dim, num_layers=num_layers, bidirectional=bidirectional, dense=dense).to(device)
-optimizer = optim.Adam(model.parameters())
-loss_fn = nn.MSELoss()
-loader = data.DataLoader(data.TensorDataset(X_train, y_train), shuffle=True, batch_size=batch_size)
+    model_y.training_2(device, dataset, loader, n_epochs=n_epochs)
 
-train_loss = []
 
-training = True
-
-if training:
-    n_epochs = 2
-    for epoch in tqdm(range(n_epochs)):
-        model.train()
-        for X_batch, y_batch in loader:
-            y_pred = model(X_batch.to(device))
-            loss = loss_fn(y_pred.to(device), y_batch.to(device))
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        # Validation
-        if epoch % 100 != 0:
-            continue
-        model.eval()
-        with torch.no_grad():
-            y_pred = model(X_train.to(device))
-            print(y_pred.shape)
-            print(y_train.shape)
-            train_rmse = np.sqrt(loss_fn(y_pred.to(device), y_train.to(device)))
-            y_pred = model(X_test.to(device))
-            test_rmse = np.sqrt(loss_fn(y_pred.to(device), y_test.to(device)))
-        print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
-        torch.save(model, 'ltsm_lookback.pt')
-
-#model = torch.load('ltsm.pt')
-model.to(device)
-model.eval()
-with torch.no_grad():
-    # shift train predictions for plotting
-    train_plot = np.ones_like(timeseries) * np.nan
-    y_pred = model(X_train.to(device))
-    train_plot[lookback:train_size] = torch.Tensor.cpu(y_pred).numpy()
-
-    # shift test predictions for plotting
-    test_plot = np.ones_like(timeseries) * np.nan
-    y_pred_test = model(X_test.to(device))
-    test_plot[train_size + lookback:len(timeseries)] = torch.Tensor.cpu(y_pred_test).numpy()
-
-# get last 20 values from test set
-prediction_minutes = months(1)
-n = days(31)
-test_values = X_test[-n:]
-
-pred_plot = np.ones((len(timeseries) + len(test_values), 1)) * np.nan
-test_values = torch.tensor(test_values).to(device)
-
-for _ in tqdm(range(prediction_minutes)):
+def evaluate_model(p_dataset, p_device, p_train_size, p_lookback, p_timeseries):
+    X_train = p_dataset[0]
+    X_test = p_dataset[2]
+    model = torch.load('ltsm.pt')
+    model.to(p_device)
+    model.eval()
     with torch.no_grad():
-        y_pred = model(test_values.to(device))
+        # shift train predictions for plotting
+        train_plot = np.ones_like(p_timeseries) * np.nan
+        y_pred = model(X_train.to(p_device))
+        train_plot[p_lookback:p_train_size] = torch.Tensor.cpu(y_pred).numpy()
 
-        try:
-            y_pred = y_pred[-lookback]
-        except IndexError:
-            print(y_pred.shape)
-            print(test_values.shape)
+        # shift test predictions for plotting
+        test_plot = np.ones_like(p_timeseries) * np.nan
+        y_pred_test = model(X_test.to(p_device))
+        test_plot[train_size + lookback:len(p_timeseries)] = torch.Tensor.cpu(y_pred_test).numpy()
 
-        y_pred = y_pred[:, None, None]
-        torch.cat((test_values, y_pred), dim=0)
-        test_values = test_values[0:, :, :]
+        plt.plot(p_timeseries, c='b')
+        plt.plot(train_plot, c='r')
+        plt.plot(test_plot, c='g')
+        plt.savefig('eval.png')
 
-np.save('predictions.npy', torch.Tensor.cpu(test_values).numpy())
-pred_plot[len(timeseries):] = np.squeeze(torch.Tensor.cpu(test_values).numpy(), axis=2)
 
-# plot
-plt.plot(timeseries, c='b')
-plt.plot(train_plot, c='r')
-plt.plot(test_plot, c='g')
-plt.plot(pred_plot, c='y')
-plt.savefig('lstm.png')
+def predict(device, prediction_minutes, dataset, timeseries):
+    X_train = dataset[0]
+    y_train = dataset[1]
+    X_test = dataset[2]
+    y_test = dataset[3]
+    model = torch.load('ltsm.pt')
+    # get last 20 values from test set
+    n = days(31)
+    test_values = X_test[-n:]
+
+    pred_plot = np.ones((len(timeseries) + len(test_values), 1)) * np.nan
+    test_values = torch.tensor(test_values).to(device)
+
+    for _ in tqdm(range(prediction_minutes)):
+        with torch.no_grad():
+            y_pred = model(test_values.to(device))
+
+            try:
+                y_pred = y_pred[-lookback]
+            except IndexError:
+                print(y_pred.shape)
+                print(test_values.shape)
+
+            y_pred = y_pred[:, None, None]
+            torch.cat((test_values, y_pred), dim=0)
+            test_values = test_values[0:, :, :]
+
+    np.save('predictions.npy', torch.Tensor.cpu(test_values).numpy())
+    pred_plot[len(timeseries):] = np.squeeze(torch.Tensor.cpu(test_values).numpy(), axis=2)
+
+    # plot
+    plt.plot(timeseries, c='b')
+    plt.plot(pred_plot, c='g')
+    plt.savefig('predict.png')
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    lookback = days(1)
+    hidden_dim = 16
+    num_layers = 1
+    bidirectional = True
+    dense = True
+    input_dim = 1
+    n_epochs = 10000
+    batch_size = lookback
+
+    dataset, train_size, test_size, timeseries = load_dataset(lookback)
+    train_model(n_epochs, dataset, device, batch_size, input_dim, hidden_dim, num_layers, bidirectional, dense)
+    evaluate_model(dataset, device, train_size, lookback, timeseries)
+    predict(device, 60, dataset, timeseries)
